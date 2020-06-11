f2737ebb409b2272d45a3eb635ee6f53444bdc73
Alexandru Vulpe <alevul@ebs-integrator.com>
2020-06-11 19:26:12 +0300

    Add react logo png

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
       <meta name="description" content="Weather forecast information" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="#3367D6" />
-      <link rel="apple-touch-icon" href="/apple-devices-icon.png"></link>
+      <link rel="apple-touch-icon" href="/images/apple-devices-icon.png" />
     </Head>
     <Component {...pageProps} />
   </>
