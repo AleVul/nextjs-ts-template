a021c2b522413cd42810d82ff8b882df4dcc84ab
Alexandru Vulpe <alevul@ebs-integrator.com>
2020-06-16 20:41:57 +0300

    Continued work

--- configs/workbox-config.js
+++ configs/workbox-config.js
@@ -1,15 +1,28 @@
 module.exports = {
-  globDirectory: ".",
-  globPatterns: ["**/*.js"],
+  //globDirectory: ".",
+  //globPatterns: ["**/*.js"],
   swDest: "public/sw.js",
+  cacheId: "nextjs-template-v1",
+  // TODO: this requires a hash
+  // dontCacheBustURLsMatching: "",
   runtimeCaching: [
     {
-      urlPattern: /^https?.*/,
+      urlPattern: /_next\/static\/[a-zA-Z0-9_-]*\/pages\/[_a-z]*\.js$/,
       handler: "NetworkFirst",
       options: {
-        cacheName: "offlineCache",
+        cacheName: "pages",
         expiration: {
           maxEntries: 200,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "images",
+        expiration: {
+          maxEntries: 10,
         },
       },
     },
